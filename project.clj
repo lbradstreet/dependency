a2955849fcf6a6036e44d673f8f6b5b3251239f7
Lucas Bradstreet <lucasbradstreet@gmail.com>
2014-03-06 11:28:09 +0800

    Start conversion to cljx

--- project.clj
+++ project.clj
@@ -3,6 +3,19 @@
   :url "https://github.com/stuartsierra/dependency"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :clj-1.4.0 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :clj-1.3.0 {:dependencies [[org.clojure/clojure "1.3.0"]]}})
+
+  :plugins [[com.keminglabs/cljx "0.3.2"]]
+  :cljx {:builds [{:source-paths ["src/cljx"]
+                   :output-path "target/classes"
+                   :rules :clj}
+
+                  {:source-paths ["src/cljx"]
+                   :output-path "target/classes"
+                   :rules :cljs}]}
+
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
+                                  [org.clojure/clojurescript "0.0-2138"]
+                                  [com.keminglabs/cljx "0.3.2"]]}
+             ;:clj-1.4.0 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             ;:clj-1.3.0 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             })
