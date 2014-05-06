0e1761e5114081b7989e1ea8b747d96a36e8aa84
Lucas Bradstreet <lucasbradstreet@gmail.com>
2014-05-06 18:37:50 +0800

    Add fork info to readme.

--- project.clj
+++ project.clj
@@ -1,21 +1,24 @@
-(defproject com.stuartsierra/dependency "0.1.2-SNAPSHOT"
+(defproject com.stuartsierra/dependency "0.1.3-SNAPSHOT"
   :description "A data structure for representing dependency graphs"
   :url "https://github.com/stuartsierra/dependency"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :plugins [[com.keminglabs/cljx "0.3.2"]]
-  :cljx {:builds [{:source-paths ["src/cljx"]
-                   :output-path "target/classes"
-                   :rules :clj}
-
-                  {:source-paths ["src/cljx"]
-                   :output-path "target/classes"
-                   :rules :cljs}]}
+  ;:resource-paths  ["target/generated/src/cljs"]
+  :source-paths  ["target/generated"]
 
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
-                                  [org.clojure/clojurescript "0.0-2138"]
-                                  [com.keminglabs/cljx "0.3.2"]]}
-             ;:clj-1.4.0 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             ;:clj-1.3.0 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             })
+                                  [org.clojure/clojurescript "0.0-2156"]
+                                  [com.keminglabs/cljx "0.3.2"]]
+                   :plugins [[com.keminglabs/cljx "0.3.2"]
+                             [com.cemerick/austin  "0.1.4"]]
+                   :prep-tasks  ["cljx"]
+                   :cljx {:builds [{:source-paths ["src"]
+                                    :output-path "target/generated"
+                                    :rules :clj}
+
+                                   {:source-paths ["src"]
+                                    :output-path "target/generated"
+                                    :rules :cljs}]}}
+             :clj-1.4.0 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :clj-1.3.0 {:dependencies [[org.clojure/clojure "1.3.0"]]}})
